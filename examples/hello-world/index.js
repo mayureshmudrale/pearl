7036a24ca5445b654a16556d8ec1cfd6462142a6
mayureshmudrale <71888845+mayureshmudrale@users.noreply.github.com>
2023-03-27 11:13:19 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,11 +5,11 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello World hello fargate');
 });
 
 /* istanbul ignore next */
 if (!module.parent) {
-  app.listen(3000);
+  app.listen(80);
   console.log('Express started on port 3000');
 }
